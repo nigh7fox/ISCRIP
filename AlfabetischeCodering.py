8ec52096ed762d3790475381aa767232d848a835
Joshua Tromp <joshwow91@gmail@github.com>
2019-03-19 16:12:21 +0100

    laatste opdracht

---
+++ AlfabetischeCodering.py
@@ -0,0 +1,18 @@
+def geldigeSleutel(woord) -> bool:
+    return False
+
+
+def codeer(woord, sleutel):
+    return 0
+
+
+def alfabetisch(woord) -> bool:
+    return True
+
+
+def score(sleutel, bestand) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    geldigeSleutel(
